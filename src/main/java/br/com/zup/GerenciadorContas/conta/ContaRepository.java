e5d0b3948dba1908f636ba5e23bf7be5e19f3eb5
Bárbara Romero <bqromero@gmail.com>
2021-11-10 11:47:54 -0300

    Criar interface Repository

---
+++ src/main/java/br/com/zup/GerenciadorContas/conta/ContaRepository.java
@@ -0,0 +1,6 @@
+package br.com.zup.GerenciadorContas.conta;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ContaRepository extends CrudRepository <Conta, Integer> {
+}
