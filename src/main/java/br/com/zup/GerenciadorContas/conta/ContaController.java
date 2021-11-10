020b1c91f74aa34798d00677ac5a50d950404043
Bárbara Romero <bqromero@gmail.com>
2021-11-10 14:05:58 -0300

    Criar modelmapper para converter em DTO em model

--- src/main/java/br/com/zup/GerenciadorContas/conta/ContaController.java
+++ src/main/java/br/com/zup/GerenciadorContas/conta/ContaController.java
@@ -1,13 +1,27 @@
 package br.com.zup.GerenciadorContas.conta;
 
 
+import br.com.zup.GerenciadorContas.conta.dtos.EntradaDTO;
+import br.com.zup.GerenciadorContas.conta.dtos.SaidaDTO;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/contas")
 public class ContaController {
-  @Autowired ContaService contaService;
+  @Autowired
+  ContaService contaService;
+
+  @Autowired
+  private ModelMapper modelMapper;
+
+  @PostMapping
+  @ResponseStatus
+  public SaidaDTO cadastrarConta(@RequestBody EntradaDTO entradaDTO) {
+    Conta conta = modelMapper.map(entradaDTO, Conta.class);
+    return modelMapper.map(contaService.cadastrarConta(conta), SaidaDTO.class);
+
+  }
 
 }
--- src/main/java/br/com/zup/GerenciadorContas/conta/ContaService.java
+++ src/main/java/br/com/zup/GerenciadorContas/conta/ContaService.java
@@ -1,6 +1,7 @@
 package br.com.zup.GerenciadorContas.conta;
 
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +9,11 @@ import org.springframework.stereotype.Service;
 public class ContaService {
   @Autowired
   private ContaRepository contaRepository;
+  @Autowired
+  private ModelMapper modelMapper;
+
+  public Conta cadastrarConta(Conta conta){
+    conta.setDataDePagamento(null);
+    return contaRepository.save(conta);
+  }
 }
