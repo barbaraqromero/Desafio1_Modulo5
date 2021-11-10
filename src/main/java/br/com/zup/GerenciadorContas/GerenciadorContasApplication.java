29babf8f2dc1028725252f7ac1902b7ce668788b
Bárbara Romero <bqromero@gmail.com>
2021-11-10 11:18:17 -0300

    Criar a classe Model

---
+++ src/main/java/br/com/zup/GerenciadorContas/GerenciadorContasApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.GerenciadorContas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorContasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorContasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/GerenciadorContas/GerenciadorContasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.GerenciadorContas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
