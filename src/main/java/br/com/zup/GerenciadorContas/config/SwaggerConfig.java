e425707dc42a5c34645e081550c918a7b900cea5
Bárbara Romero <bqromero@gmail.com>
2021-11-12 18:56:00 -0300

    Atualizar validação para nome e tipo

--- src/main/java/br/com/zup/GerenciadorContas/config/SwaggerConfig.java
+++ src/main/java/br/com/zup/GerenciadorContas/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package br.com.zup.GerenciadorContas.config;
 
-import br.com.zup.GerenciadorContas.GerenciadorContasApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
--- src/main/java/br/com/zup/GerenciadorContas/conta/dtos/EntradaDTO.java
+++ src/main/java/br/com/zup/GerenciadorContas/conta/dtos/EntradaDTO.java
@@ -3,16 +3,19 @@ package br.com.zup.GerenciadorContas.conta.dtos;
 import br.com.zup.GerenciadorContas.conta.enums.Tipo;
 
 import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 public class EntradaDTO {
 
+  @NotNull
   @Size(min = 2, message = "Digite um nome com pelo menos 2 caracteres")
   private String nome;
   @DecimalMin(value = "0.01", message = "Digite um valor acima de 0")
   private double valor;
+  @NotNull
   private Tipo tipo;
   @NotNull(message = "O campo 'Data de vencimento' deve ser preenchido")
   private LocalDate dataDeVencimento;
